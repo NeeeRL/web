11a1437b541cdc9d1619bced1c21e94e5a6064f2
NeeeRL <kenken.s5049@gmail.com>
2024-11-19 12:56:14 +0900

    とりあえず左をクリックすると戻るようにした。

--- web/js/usual/headerAndFooter.js
+++ web/js/usual/headerAndFooter.js
@@ -4,3 +4,12 @@ fetch("/header.html")
 fetch("/footer.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#footer").innerHTML = data);
+
+document.addEventListener("DOMContentLoaded", () => {
+    const Menu_checkbox = document.getElementById("menu-btn-check");
+const Menu_space = document.getElementById("menu-space");
+
+Menu_space.addEventListener('click', function() {
+    Menu_checkbox.checked = !Menu_checkbox.checked
+});
+});
